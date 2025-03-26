b99e34205986a5d6f2f00c1c34232e2f8582f920
JohnCLodge <labadgett2@gmail.com>
2025-03-26 08:26:58 -0400

    Initial Commit

--- LilyTimer/ContentView.swift
+++ LilyTimer/ContentView.swift
@@ -11,8 +11,10 @@ struct ContentView: View {
     
     @State private var isStarted: Bool = false
     @State private var isPaused: Bool = false
+    @State private var showAlert: Bool = false
+    @State private var alertMessage: String = ""
+    @State private var isSuccess: Bool = false
     
-        
     var body: some View {
         NavigationView {
             ZStack {
@@ -21,92 +23,104 @@ struct ContentView: View {
                 Text("Edit Timer Area")
                     .foregroundStyle(.white)
             }
-            ZStack{
-                Color.black
-                    .ignoresSafeArea()
-                VStack{
-                    Text("Timer Placeholder")
-                        .font(.system(size: 48) .weight(.thin))
-                    
-                    Spacer()
-                    
-                    Image("Stage1")
-                    
-                    Spacer()
-                    
-                    if isStarted {
-                        HStack{
+        }
+        ZStack{
+            Color.black
+                .ignoresSafeArea()
+            VStack{
+                Text("Timer Placeholder")
+                    .font(.system(size: 48) .weight(.thin))
+                
+                Spacer()
+                
+                Image("Stage1")
+                
+                Spacer()
+                
+                if isStarted {
+                    HStack{
+                        
+                        // Pause Button
+                        Button{
+                            //Action
+                            isPaused.toggle()
                             
-                            // Pause Button
-                            Button{
-                                //Action
-                                isPaused.toggle()
-                                
-                                //toggle timer
-                            }
-                            label: {
-                                if isPaused {
-                                    ZStack{
-                                        RoundedRectangle(cornerRadius: 24)
-                                            .frame(width: 90, height: 90)
-                                            .foregroundStyle(.gray)
-                                        Image(systemName: "play.fill")
-                                            .font(.system(size: 48) .weight(.thin))
-                                    }
-                                }
-                                else {
-                                    ZStack{
-                                        RoundedRectangle(cornerRadius: 24)
-                                            .frame(width: 90, height: 90)
-                                            .foregroundStyle(.gray)
-                                        Image(systemName: "pause")
-                                            .font(.system(size: 48) .weight(.thin))
-                                    }
-                                }
-                            }
-                            .padding(.horizontal, 50)
-                            
-                            // Stop Button
-                            Button{
-                                
-                                //Action
-                                isStarted.toggle()
-                            }
-                            label: {
+                            //toggle timer
+                        }
+                        label: {
+                            if isPaused {
                                 ZStack{
                                     RoundedRectangle(cornerRadius: 24)
                                         .frame(width: 90, height: 90)
-                                        .foregroundStyle(.red)
-                                    Image(systemName: "stop.circle")
+                                        .foregroundStyle(.gray)
+                                    Image(systemName: "play.fill")
                                         .font(.system(size: 48) .weight(.thin))
                                 }
                             }
-                            .padding(.horizontal, 50)
-
+                            else {
+                                ZStack{
+                                    RoundedRectangle(cornerRadius: 24)
+                                        .frame(width: 90, height: 90)
+                                        .foregroundStyle(.gray)
+                                    Image(systemName: "pause")
+                                        .font(.system(size: 48) .weight(.thin))
+                                }
+                            }
                         }
-                    }
-                    // Start Button
-                    else{
+                        .padding(.horizontal, 50)
+                        
+                        // Stop Button
                         Button{
+                            
                             //Action
                             isStarted.toggle()
-                        } label: {
+                        }
+                        label: {
                             ZStack{
                                 RoundedRectangle(cornerRadius: 24)
-                                    .frame(width: 310, height: 90)
-                                    .foregroundStyle(.gray)
-                                Text("Start")
+                                    .frame(width: 90, height: 90)
+                                    .foregroundStyle(.red)
+                                Image(systemName: "stop.circle")
                                     .font(.system(size: 48) .weight(.thin))
                             }
                         }
+                        .padding(.horizontal, 50)
+                        
                     }
                 }
-                .padding()
+                // (here3)Start Button
+                else{
+                    Button{
+                        //Action
+                        isStarted.toggle()
+                    } label: {
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 24)
+                                .frame(width: 310, height: 90)
+                                .foregroundStyle(.gray)
+                            Text("Start")
+                                .font(.system(size: 48) .weight(.thin))
+                        }
+                    }
+                }//
+                alert(isPresented: $showAlert) {
+                    if isSuccess {
+                        return Alert(title: Text("Hey"), message:
+                                        Text("Are you sure you want to stop!"), dismissButton:
+                                .default(Text("OK")))
+                    } else {
+                        return Alert(title: Text ("Enjoy Your Break"), message:
+                                        Text(alertMessage), dismissButton: .default(Text("OK")))
+                    }
+                }
             }
+            
         }
         .foregroundStyle(.white)
     }
+        
 }
+
 
 #Preview {
     ContentView()
