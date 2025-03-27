9a10d076e886100e36b69fb95c77c0a8e39bf79a
Joseph Brinker <joey.brinker805@outlook.com>
2025-03-27 09:49:41 -0400

    Notifications

--- LilyTimer/ContentView.swift
+++ LilyTimer/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
     
@@ -15,6 +16,7 @@ struct ContentView: View {
     @State private var showAlert: Bool = false
     @State private var alertMessage: String = ""
     @State private var isSuccess: Bool = false
+    @State private var triedToStop = false
     
     // Timer that updates every 1 second
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
@@ -39,150 +41,210 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            ZStack {
-                Color.gray
+            
+            setTimerView
+            
+            ZStack{
+                Color.black
                     .ignoresSafeArea()
-                VStack(spacing: 100){
-                    VStack{
-                        // Work time slider
-                        Text("Work Time")
-                        Slider(value: $workTime, in: 0...60, step: 1)
-                            .padding()
-                        Text("\(Int(workTime / 60)) Minutes")
+                VStack{
+                    if !breakIsStarted {
+                        Text(workIsStarted ? "\(Int(remainingTime / 60)):\(String(format: "%02d" ,(Int(remainingTime) % 60)))" : "Start")
+                            .font(.system(size: 48) .weight(.thin))
                     }
-                    VStack{
-                        // Break time slider
-                        Text("Break Time")
-                        Slider(value: $breakTime, in: 0...60, step: 1)
-                            .padding()
-                        Text("\(Int(breakTime / 60)) Minutes")
+                    else {
+                        Text("\(Int(remainingBreakTime / 60)):\(Int(remainingBreakTime) % 60)")
+                            .font(.system(size: 48) .weight(.thin))
                     }
-                }
-            }
-        }
-        ZStack{
-            Color.black
-                .ignoresSafeArea()
-            VStack{
-                if !breakIsStarted {
-                    Text(workIsStarted ? "\(Int(remainingTime / 60)):\(Int(remainingTime) % 60)" : "Start")
-                        .font(.system(size: 48) .weight(.thin))
-                }
-                else {
-                    Text("\(Int(remainingBreakTime / 60)):\(Int(remainingBreakTime) % 60)")
-                        .font(.system(size: 48) .weight(.thin))
-                }
-                
-                Spacer()
-                
-                if breakIsStarted {
-                    Image("Stage5")
-                } else {
-                    Image(workIsStarted ? currentImageName : "Stage1")
-                }
-                
-                Spacer()
-                
-                // Show controls only while work timer is active
-                if workIsStarted {
-                    HStack {
-                        // Pause/Play Button
-                        Button {
-                            isPaused.toggle()
-                        } label: {
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 24)
-                                    .frame(width: 90, height: 90)
-                                    .foregroundStyle(.gray)
-                                Image(systemName: isPaused ? "play.fill" : "pause")
-                                    .font(.system(size: 48).weight(.thin))
-                            }
-                        }
-                        .padding(.horizontal, 50)
-                        
-                        // Stop Button
-                        Button {
-                            // Stop work timer and reset times
-                            workIsStarted = false
-                            remainingTime = workTime
-                            remainingBreakTime = breakTime
-                        } label: {
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 24)
-                                    .frame(width: 90, height: 90)
-                                    .foregroundStyle(.red)
-                                Image(systemName: "stop.circle")
-                                    .font(.system(size: 48).weight(.thin))
-                            }
-                        }
-                        .padding(.horizontal, 50)
-                    }
-                }
-                // Show start button only if neither timer is active.
-                else if !breakIsStarted {
-                    Button {
-                        workIsStarted = true
-                        isPaused = false
-                        remainingTime = workTime
-                        remainingBreakTime = breakTime
-                    } label: {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 24)
-                                .frame(width: 310, height: 90)
-                                .foregroundStyle(.gray)
-                            Text("Start")
-                                .font(.system(size: 48).weight(.thin))
-                        }
-                    }
-                    .padding(.horizontal, 50)
                     
-                }
-                // (here3)Start Button
-                else{
-                    Button{
-                        //Action
-                        workIsStarted.toggle()
-                    } label: {
-                        ZStack{
-                            RoundedRectangle(cornerRadius: 24)
-                                .frame(width: 310, height: 90)
-                                .foregroundStyle(.gray)
-                            Text("Start")
-                                .font(.system(size: 48) .weight(.thin))
+                    Spacer()
+                    
+                    if breakIsStarted {
+                        Image("Stage5")
+                    } else {
+                        Image(workIsStarted ? currentImageName : "Stage1")
+                    }
+                    
+                    Spacer()
+                    
+                    // Show controls only while work timer is active
+                    if workIsStarted {
+                        pauseAndStop
+                    }
+                    // Show start button only if neither timer is active.
+                    else if !breakIsStarted {
+                        startButton
+                        
+                    }
+                    // (here3)Start Button
+                    else {
+                        Button{
+                            //Action
+                            workIsStarted.toggle()
+                        } label: {
+                            
+                        }
+                        .alert(isPresented: $showAlert) {
+                            Alert(title: Text ("Enjoy Your Break"), message:
+                                    Text(alertMessage), dismissButton: .default(Text("OK")))
                         }
                     }
-                }//
-                alert(isPresented: $showAlert) {
-                    if isSuccess {
-                        return Alert(title: Text("Hey"), message:
-                                        Text("Are you sure you want to stop!"), dismissButton:
-                                .default(Text("OK")))
-                    } else {
-                        return Alert(title: Text ("Enjoy Your Break"), message:
-                                        Text(alertMessage), dismissButton: .default(Text("OK")))
+                }
+                .foregroundStyle(.white)
+                .onReceive(timer) { _ in
+                    if !isPaused {
+                        if workIsStarted && remainingTime > 0 {
+                            remainingTime -= 1
+                        } else if workIsStarted && remainingTime <= 0 {
+                            // When work timer ends, switch to break timer
+                            workIsStarted = false
+                            breakIsStarted = true
+                            showAlert = true
+                            workOverNotification()
+                        } else if breakIsStarted && remainingBreakTime > 0 {
+                            remainingBreakTime -= 1
+                        } else if breakIsStarted && remainingBreakTime <= 0 {
+                            // When break timer ends, no timers are running
+                            breakIsStarted = false
+                            breakOverNotification()
+                        }
                     }
                 }
-                .font(.system(size: 48) .weight(.thin))
-                .padding()
-            }
-            .foregroundStyle(.white)
-            .onReceive(timer) { _ in
-                if !isPaused {
-                    if workIsStarted && remainingTime > 0 {
-                        remainingTime -= 1
-                    } else if workIsStarted && remainingTime <= 0 {
-                        // When work timer ends, switch to break timer
-                        workIsStarted = false
-                        breakIsStarted = true
-                    } else if breakIsStarted && remainingBreakTime > 0 {
-                        remainingBreakTime -= 1
-                    } else if breakIsStarted && remainingBreakTime <= 0 {
-                        // When break timer ends, no timers are running
-                        breakIsStarted = false
+                .onAppear{
+                    UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+                        if granted {
+                            print("Notification permission granted.")
+                        } else if let error = error {
+                            print("Error requesting notification permission: \(error.localizedDescription)")
+                        }
                     }
                 }
             }
         }
+    }
+    
+    private var setTimerView: some View {
+        ZStack {
+            Color.gray
+                .ignoresSafeArea()
+            VStack(spacing: 100){
+                VStack{
+                    // Work time slider
+                    Text("Work Time")
+                    Slider(value: $workTime, in: 0...20, step: 1)
+                        .padding()
+                    Text("\(Int(workTime / 60)) Minutes")
+                }
+                VStack{
+                    // Break time slider
+                    Text("Break Time")
+                    Slider(value: $breakTime, in: 0...20, step: 1)
+                        .padding()
+                    Text("\(Int(breakTime / 60)) Minutes")
+                }
+            }
+        }
+    }
+    
+    private var startButton: some View {
+        Button {
+            workIsStarted = true
+            remainingTime = workTime
+            remainingBreakTime = breakTime
+        } label: {
+            ZStack {
+                RoundedRectangle(cornerRadius: 24)
+                    .frame(width: 310, height: 90)
+                    .foregroundStyle(.gray)
+                Text("Start")
+                    .font(.system(size: 48).weight(.thin))
+            }
+        }
+        .padding(.horizontal, 50)
+    }
+    
+    private var pauseAndStop: some View {
+        HStack {
+            // Pause/Play Button
+            Button {
+                isPaused.toggle()
+            } label: {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 24)
+                        .frame(width: 90, height: 90)
+                        .foregroundStyle(.gray)
+                    Image(systemName: isPaused ? "play.fill" : "pause")
+                        .font(.system(size: 48).weight(.thin))
+                }
+            }
+            
+            .padding(.horizontal, 50)
+            
+            // Stop Button
+            Button {
+                // Stop work timer and reset times
+                triedToStop = true
+            } label: {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 24)
+                        .frame(width: 90, height: 90)
+                        .foregroundStyle(.red)
+                    Image(systemName: "stop.circle")
+                        .font(.system(size: 48).weight(.thin))
+                }
+            }
+            .alert(isPresented: $triedToStop) {
+                Alert(title: Text("Are you sure?"), message: Text("You will lose all progress."), primaryButton: .default(Text("Cancel")), secondaryButton: .destructive(Text("Stop")) {
+                    workIsStarted = false
+                    breakIsStarted = false
+                    isPaused = false
+                })
+            }
+            // Warning Alert
+            .padding(.horizontal, 50)
+        }
+    }
+    
+    func workOverNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "Work time is over!"
+        content.body = "Time to take a break for \(String(format: "%.0f", breakTime)) minutes!"
+        content.sound = .default
+        
+        // Trigger the notification after 1 second.
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+        
+        // Create a unique identifier for the request.
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        // Add the notification request to the notification center.
+        UNUserNotificationCenter.current().add(request) { error in
+            if let error = error {
+                print("Error scheduling notification: \(error.localizedDescription)")
+            }
+        }
+        print("done")
+    }
+    
+    func breakOverNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "Break time is over!"
+        content.body = "Hope you had a good break! Time to start your work again!"
+        content.sound = .default
+        
+        // Trigger the notification after 1 second.
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+        
+        // Create a unique identifier for the request.
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        // Add the notification request to the notification center.
+        UNUserNotificationCenter.current().add(request) { error in
+            if let error = error {
+                print("Error scheduling notification: \(error.localizedDescription)")
+            }
+        }
+        print("done")
     }
 }
 
